ed798ae3f1c676a5ca4963a2012480db26aa8f7a
Robert Laws <roblaws@me.com>
2017-10-29 15:31:58 +0000

    automatically backed up by learn

--- db/migrate/01_create_recipes.rb
+++ db/migrate/01_create_recipes.rb
@@ -3,7 +3,7 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
       t.string :name
       t.string :ingredients
-      t.sting :cook_time
+      t.string :cook_time
     end
   end
 end
